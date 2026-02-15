7a6dd5b52de7369cc051a44b5d72f3c1475c54a7
Nasrul78 <nasrulillah78@gmail.com>
2026-02-15 11:24:09 +0700

    initial commit

---
+++ src/main/java/io/github/nasrul78/cinelogue_api/CinelogueApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nasrul78.cinelogue_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinelogueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinelogueApiApplication.class, args);
+	}
+
+}
